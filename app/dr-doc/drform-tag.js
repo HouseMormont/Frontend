fd3f906bbfe102b6f6f3a4b579b07f79d8801dec
lucibr <lucian.bredean@outlook.com>
2017-01-04 20:26:03 +0200

    HTML&CSS - Dispozitia rectorului form

---
+++ app/dr-doc/drform-tag.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Lucian Bredean on 1/3/2017.
+ */
+angular.module('EasyDocsUBBApp')
+    .component('drformTag', {
+        templateUrl: 'dr-doc/drform-tag.html',
+        controller: function () {
+            $ctrl = this;
+
+        }
+    });
